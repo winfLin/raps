ea873ab5e2cd73d801da09ccd08b3bcffc559918
winfLin <92909885+winfLin@users.noreply.github.com>
2024-05-07 22:11:14 +0800

    v2

---
+++ print/php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7"/>
+	<META content="text/html; charset=utf-8" http-equiv="Content-Type"/>  	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Photo Frame</title>
+</head>
+<body>		
+<?php
+	echo "123";
+	
+?>
+</body>
+</html>
